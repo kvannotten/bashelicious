16eb69814b8a413260341d389df8c0d74867ef11
Kristof <kristof@vannotten.be>
2012-07-15 14:43:37 +0200

    (closes #10) Added bashelicious function with some methods (needs to be expanded)
    unset the bashelicious_import function

--- lib/functions.sh
+++ lib/functions.sh
@@ -5,3 +5,29 @@ function findproc() { ps aux | grep -i $1 | grep -v grep ;}
 function parse_git_branch_and_add_brackets {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
 }
+
+function bashelicious {
+	bold=`tput bold`;
+	normal=`tput sgr0`;
+	case "$1" in
+		reload)
+			[[ -e $HOME/.bashrc ]] && . $HOME/.bashrc
+			;;
+		list)
+			echo "${bold}Plugins for Bashelicous${normal}";
+			FILES="$HOME/.bashelicious/plugins"
+			for i in $FILES/*
+			do
+				if [[ -f $i ]]; then
+					j=${i##*/}
+					j=${j%.plugin.sh}
+					echo " [*] ${j}"
+				fi
+			done
+			echo ""
+			;;
+		*)
+			echo "Usage: ${bold}bashelicous${normal} {reload list}"
+			;;
+	esac
+}

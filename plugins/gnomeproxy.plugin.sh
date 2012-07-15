43c045b4e4f603c23817df430768ccc27f50db94
benvh <ben.vanhoutven@gmail.com>
2012-07-15 15:32:52 +0200

    Added uname check to the gnomeproxy plugin so it doesn't try to call gsettings on osx

--- plugins/gnomeproxy.plugin.sh
+++ plugins/gnomeproxy.plugin.sh
@@ -2,33 +2,36 @@
 # Bashelicious gnome proxy plugin #
 ###################################
 
-function dequote() {
-	echo $1 | sed -e "s|'||g"	
-}
-
-function desquarebracket() {
-	echo $1 | sed -E -e "s/(\[|\])//g"
-}
-
-PROXY_MODE=$(gsettings get org.gnome.system.proxy mode)
-if [[ $(dequote $PROXY_MODE) == "manual" ]]
+if [[ $(uname) != "Darwin" ]]
 then
-	HTTP_HOST=$(dequote $(gsettings get org.gnome.system.proxy.http host))
-	HTTP_PORT=$(gsettings get org.gnome.system.proxy.http port)
+	function dequote() {
+		echo $1 | sed -e "s|'||g"	
+	}
 
-	HTTPS_HOST=$(dequote $(gsettings get org.gnome.system.proxy.https host))
-	HTTPS_PORT=$(gsettings get org.gnome.system.proxy.https port)
+	function desquarebracket() {
+		echo $1 | sed -E -e "s/(\[|\])//g"
+	}
 
-	FTP_HOST=$(dequote $(gsettings get org.gnome.system.proxy.ftp host))
-	FTP_PORT=$(gsettings get org.gnome.system.proxy.ftp port)
+	PROXY_MODE=$(gsettings get org.gnome.system.proxy mode)
+	if [[ $(dequote $PROXY_MODE) == "manual" ]]
+	then
+		HTTP_HOST=$(dequote $(gsettings get org.gnome.system.proxy.http host))
+		HTTP_PORT=$(gsettings get org.gnome.system.proxy.http port)
 
-	IGNORE_HOSTS=$(desquarebracket "$(gsettings get org.gnome.system.proxy ignore-hosts)")
+		HTTPS_HOST=$(dequote $(gsettings get org.gnome.system.proxy.https host))
+		HTTPS_PORT=$(gsettings get org.gnome.system.proxy.https port)
 
-	export http_proxy="$HTTP_HOST:$HTTP_PORT"
-	export https_proxy="$HTTPS_HOST:$HTTPS_PORT"
-	export ftp_proxy="$FTP_HOST:$FTP_PORT"
-	export no_proxy="$IGNORE_HOSTS"
+		FTP_HOST=$(dequote $(gsettings get org.gnome.system.proxy.ftp host))
+		FTP_PORT=$(gsettings get org.gnome.system.proxy.ftp port)
+
+		IGNORE_HOSTS=$(desquarebracket "$(gsettings get org.gnome.system.proxy ignore-hosts)")
+
+		export http_proxy="$HTTP_HOST:$HTTP_PORT"
+		export https_proxy="$HTTPS_HOST:$HTTPS_PORT"
+		export ftp_proxy="$FTP_HOST:$FTP_PORT"
+		export no_proxy="$IGNORE_HOSTS"
+	fi
+
+	unset -f dequote
+	unset -f desquarebracket
 fi
-
-unset -f dequote
-unset -f desquarebracket
